8f0dfc01b6c1ed984bf6937120809d2d98fbbf60
cvignat <126682957+cvignat@users.noreply.github.com>
2023-03-14 17:59:12 +0200

    Delete research_files directory

--- research_files/research.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20180929
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('research_files/researchMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
