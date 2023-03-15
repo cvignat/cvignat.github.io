d643189bdeece323a68c85ba0e22f90d70cf5ed9
cvignat <126682957+cvignat@users.noreply.github.com>
2023-03-15 17:31:15 +0200

    Add files via upload

---
+++ tree_of_life_files/tree_of_life.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20141108
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('tree_of_life_files/tree_of_lifeMoz.css')
+Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
